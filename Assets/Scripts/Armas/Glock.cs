6e594eccc6be7235e8c6918c1d2d8a7f3beec1ca
Alex Rosa <alexr@gec.inatel.br>
2020-06-27 19:26:07 -0300

    melhorando trajetoria da bala

--- Assets/Scripts/Armas/Glock.cs
+++ Assets/Scripts/Armas/Glock.cs
@@ -47,7 +47,8 @@ public class Glock : MonoBehaviour
         GameObject efeitoTiroObj = Instantiate(efeitoTiro, posEfeitoTiro.transform.position, posEfeitoTiro.transform.rotation);
         efeitoTiroObj.transform.parent = posEfeitoTiro.transform;
 
-        if (Physics.SphereCast(ray, 0.1f, out hit))
+        if (Physics.Raycast(new Vector3(ray.origin.x + Random.Range(-0.05f, 0.05f), ray.origin.y + Random.Range(-0.05f, 0.05f), ray.origin.z), 
+            Camera.main.transform.forward, out hit))
         {
             InstanciaEfeitos();
 
