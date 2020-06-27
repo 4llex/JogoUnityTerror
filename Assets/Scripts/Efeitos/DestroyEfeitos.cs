698105e1081be1334a17968785307a38e890093a
Alex Rosa <alexr@gec.inatel.br>
2020-06-27 19:11:56 -0300

    destruindo efeitos/gameObjects em cena

---
+++ Assets/Scripts/Efeitos/DestroyEfeitos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEfeitos : MonoBehaviour
+{
+    public float tempo = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, tempo);
+    }
+
+   
+}
