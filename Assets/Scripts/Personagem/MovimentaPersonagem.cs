b9408bce357b53981fc300d9e8cd03b78feb6c5c
Alex Rosa <alexr@gec.inatel.br>
2020-06-29 18:40:27 -0300

    suavizando o movemento de abaixar

--- Assets/Scripts/Personagem/MovimentaPersonagem.cs
+++ Assets/Scripts/Personagem/MovimentaPersonagem.cs
@@ -27,6 +27,8 @@ namespace BASA {
         public float alturaLevantado, alturaAbaixado, posicaoCameraEmPe, posicaoCameraAbaixado;
         RaycastHit hit;
 
+        float velocidadeCorrente = 1f;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -44,11 +46,22 @@ namespace BASA {
         // Update is called once per frame
         void Update()
         {
+            Verificacoes();
+            MovimentoAbaixa();
+            Inputs();
+       
+        }
+
+        /// <summary>
+        /// metodo de verificacoes
+        /// </summary>
+        void Verificacoes()
+        {
             // para saber se personagen esta tocando no chao e assim poder pular
             estaNoChao = Physics.CheckSphere(checaChao.position, raioEsfera, chaoMask);
 
             // suaviza a velocidade de queda apos o pulo
-            if(estaNoChao && velocidadeCai.y < 0)
+            if (estaNoChao && velocidadeCai.y < 0)
             {
                 velocidadeCai.y = -2f;
             }
@@ -60,27 +73,50 @@ namespace BASA {
             Vector3 move = (transform.right * x + transform.forward * z).normalized;
             controle.Move(move * velocidade * Time.deltaTime);
 
-            //fazer pular
-            if (Input.GetButtonDown("Jump") && estaNoChao)
-            {
-                velocidadeCai.y = Mathf.Sqrt(alturaPulo * -2f * gravidade);
-            }
+
             velocidadeCai.y += gravidade * Time.deltaTime;
-        
-            controle.Move(velocidadeCai * Time.deltaTime);
 
-            //verifica se o personagem esta abaixado
-            if (estaAbaixado)
-            {
-                ChecaBloqueioAbaixado();
-            }
+            controle.Move(velocidadeCai * Time.deltaTime);           
 
+        }
+
+        void Inputs()
+        {
             //chama metodo para abaixar
             if (Input.GetKeyDown(KeyCode.LeftControl))
             {
                 Abaixa();
             }
-       
+
+            //fazer pular
+            if (Input.GetButtonDown("Jump") && estaNoChao)
+            {
+                velocidadeCai.y = Mathf.Sqrt(alturaPulo * -2f * gravidade);
+            }
+        }
+
+        void MovimentoAbaixa()
+        {
+            controle.center = Vector3.down * (alturaLevantado - controle.height) / 2f;
+
+            if (estaAbaixado)
+            {
+                controle.height = Mathf.Lerp(controle.height, alturaAbaixado, Time.deltaTime * 3);
+                float novoY = Mathf.SmoothDamp(cameraTransform.localPosition.y, posicaoCameraAbaixado, ref velocidadeCorrente, Time.deltaTime * 3);
+                cameraTransform.localPosition = new Vector3(0, novoY, 0);
+                velocidade = 3f;
+                //verifica se o personagem esta abaixado
+                ChecaBloqueioAbaixado();
+            }
+            else
+            {
+                controle.height = Mathf.Lerp(controle.height, alturaLevantado, Time.deltaTime * 3);
+                float novoY = Mathf.SmoothDamp(cameraTransform.localPosition.y, posicaoCameraEmPe, ref velocidadeCorrente, Time.deltaTime * 3);
+                cameraTransform.localPosition = new Vector3(0, novoY, 0);
+                velocidade = 6f;
+                //verifica se o personagem esta abaixado
+                ChecaBloqueioAbaixado();
+            }
         }
 
         //faz a esfera aparecer
@@ -100,16 +136,7 @@ namespace BASA {
             }
 
             estaAbaixado = !estaAbaixado;
-            if (estaAbaixado)
-            {
-                controle.height = alturaAbaixado;
-                cameraTransform.localPosition = new Vector3(0, posicaoCameraAbaixado, 0);
-            }
-            else
-            {
-                controle.height = alturaLevantado;
-                cameraTransform.localPosition = new Vector3(0, posicaoCameraEmPe, 0);
-            }
+            
         }
 
         //metodo que verifica se tem algo em cima e pode levantar
