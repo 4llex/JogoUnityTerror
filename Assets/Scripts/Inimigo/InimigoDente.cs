8d0765e712d5c3e5f0e851da6341f072e095c17c
Alex Rosa <alexr@gec.inatel.br>
2020-06-30 21:39:58 -0300

    construindo inimigo - parte 2

---
+++ Assets/Scripts/Inimigo/InimigoDente.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class InimigoDente : MonoBehaviour
+{
+    public NavMeshAgent navMesh;
+    public GameObject player;
+    public float distanciaDoAtaque;
+    public float distanciaDoPlayer;
+    public float velocidade = 5;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanciaDoPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        VaiAtrasJogador()
+;    }
+
+    void VaiAtrasJogador()
+    {
+        navMesh.speed = velocidade;
+        if(distanciaDoPlayer < distanciaDoAtaque)
+        {
+            navMesh.isStopped = true;
+            Debug.Log("Atacando");
+        }
+        else
+        {
+            navMesh.isStopped = false;
+            navMesh.SetDestination(player.transform.position);
+        }
+    }
+}
